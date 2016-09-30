ff628a1096ef278c679b24a8dd10737f51f2ccf7
Raincal <cyj94228@gmail.com>
2016-09-30 14:32:46 +0800

    AoT

--- 005-icon/src/app/app.component.ts
+++ 005-icon/src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+  	trigger('show', [
+  		state('*', style({
+  			opacity: 1
+  		})),
+  		transition('void => *', [
+  			style({
+  				opacity: .01
+  			}),
+  			animate(500)
+  		])
+  	])
+  ]
 })
 export class AppComponent {
 }
