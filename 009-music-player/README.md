b98b8ff1144673550aa1596be5d47a8ba128867f
Raincal <cyj94228@gmail.com>
2016-10-07 08:41:33 +0800

    ｄ(･∀･*)♪ﾟ

---
+++ 009-music-player/README.md
@@ -0,0 +1,18 @@
+<p align="center">
+  <a href="http://music-player.raincal.top">
+    <img src="./008-music-player.jpg"/>
+    <br />
+    Live Demo
+  </a>
+</p>
+
+## Installation
+```bash
+npm i -g angular-cli
+npm i
+```
+
+## Start
+```bash
+ng serve
+```
--- 009-music-player/src/app/app.component.ts
+++ 009-music-player/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Music } from './music';
 
 @Component({
@@ -6,12 +6,11 @@ import { Music } from './music';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   playing: Boolean = false;
-  title: string;
-  artist: string;
-  artwork: string = 'http://y.gtimg.cn/music/photo_new/T002R300x300M000000ozKnt4SROKF.jpg?max_age=2592000';
-  audioSrc: string;
+  currentMusic: Music;
+  musicNum: number;
+  $audio: HTMLAudioElement;
   tracks: Music[] = [
     {
       id: 1,
@@ -19,27 +18,67 @@ export class AppComponent {
       artwork: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000000ozKnt4SROKF.jpg?max_age=2592000',
       audioSrc: 'http://thirdparty.gtimg.com/C200001aTDok32S3qU.m4a?vkey=B1F5CED06AC066AA848190CD9517B18C3FB44D66CFC0DC4CEF92733828D9AFBDE8B5091325F9562A215684D226E0F34073730753B06914FB&guid=9646701960&fromtag=30',
       duration: '03:34'
-    }
+    },
+    {
+      id: 2,
+      title: '薔薇のように咲いて 桜のように散って',
+      artwork: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000000UWpBa2108fE.jpg?max_age=2592000',
+      audioSrc: 'http://ws.stream.qqmusic.qq.com/C200001KUmFq4ToqB4.m4a?vkey=B1F5CED06AC066AA848190CD9517B18C3FB44D66CFC0DC4CEF92733828D9AFBDE8B5091325F9562A215684D226E0F34073730753B06914FB&guid=9646701960&fromtag=30',
+      duration: '04:22'
+    },
+    {
+      id: 3,
+      title: 'DAN DAN 心魅かれてく',
+      artwork: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000000pKT1D4c6ORv.jpg?max_age=2592000',
+      audioSrc: 'http://thirdparty.gtimg.com/C200000BgGHm0K18aU.m4a?vkey=531527658B536A19D810AC91A6886FC0C4BADA3F2734EA77E4EAAAE5CA3D9BA073065D6C911003C081557371B4D80D1C408B96B18D254B78&guid=9646701960&fromtag=30',
+      duration: '03:33'
+    },
   ];
 
-  constructor() {
+  ngOnInit() {
+    this.$audio = document.querySelector('audio');
+    this.currentMusic = this.tracks[0];
+    this.musicNum = this.tracks.length;
   }
 
   setStyles() {
     let styles = {
-      'background-image': `url(${this.artwork})`
+      'background-image': `url(${this.currentMusic.artwork})`
     };
 
     return styles;
   }
 
-  handleClick() {
-    let $audio = document.querySelector('audio');
+  play(): void {
     if (this.playing) {
-      $audio.pause();
+      this.$audio.pause();
     } else {
-      $audio.play();
+      this.$audio.play();
     }
     this.playing = !this.playing;
+  }
+
+  onSelect(track: Music): void {
+    this.currentMusic = track;
+    setTimeout(() => {
+      this.$audio.play();
+      this.playing = true;
+    }, 0);
+  }
+
+  prev(id) {
+    if (id === 1) {
+      this.onSelect(this.tracks[this.musicNum - 1]);
+    } else {
+      this.onSelect(this.tracks[id - 2]);
+    }
+  }
+
+  next(id) {
+    if (id < this.musicNum) {
+      this.onSelect(this.tracks[id]);
+    } else {
+      this.onSelect(this.tracks[0]);
+    }
   }
 }
